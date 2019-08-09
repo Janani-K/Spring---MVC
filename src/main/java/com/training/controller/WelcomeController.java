f31a991d61fb11d1e3211867ff8832f73798b91a
Janani Kandasamy <44525145+Janani-K@users.noreply.github.com>
2019-08-09 15:08:58 +0530

    Add files via upload

---
+++ src/main/java/com/training/controller/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.training.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+	
+	@RequestMapping("/")
+	public String index() {
+		
+		return "index";
+		
+	}
+
+}
